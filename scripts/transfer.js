8e25b85fb5262bebe51d183a1396c8dac2b1ac13
vikasingh700 <vikasingh700795@gmail.com>
2024-07-27 12:43:04 +0000

    Initial commit

---
+++ scripts/transfer.js
@@ -0,0 +1,33 @@
+const hre = require("hardhat");
+const { encryptDataField, decryptNodeResponse } = require("@swisstronik/utils");
+const sendShieldedTransaction = async (signer, destination, data, value) => {
+  const rpcLink = hre.network.config.url;
+  const [encryptedData] = await encryptDataField(rpcLink, data);
+  return await signer.sendTransaction({
+    from: signer.address,
+    to: destination,
+    data: encryptedData,
+    value,
+  });
+};
+
+async function main() {
+  const replace_contractAddress = "0x3FF82b52b0eDd6feB583DC3EA42598b3CBB8DD07";
+  const [signer] = await hre.ethers.getSigners();
+
+  const replace_contractFactory = await hre.ethers.getContractFactory("TestToken");
+  const contract = replace_contractFactory.attach(replace_contractAddress);
+
+  const replace_functionName = "transfer";
+  const replace_functionArgs = ["0x16af037878a6cAce2Ea29d39A3757aC2F6F7aac1", "1"];
+  const transaction = await sendShieldedTransaction(signer, replace_contractAddress, contract.interface.encodeFunctionData(replace_functionName, replace_functionArgs), 0);
+
+  await transaction.wait();
+  console.log("Transaction Response: https://explorer-evm.testnet.swisstronik.com/tx/" + transaction.hash);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
+
